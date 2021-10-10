03c13ab301cb27bea81519cbe13ad228dcfbc11a
techbysj <jsabood19@gmail.com>
2021-10-10 19:22:46 +0100

    Final Review and Testing

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -6,11 +6,11 @@ const PopularProducts = () => {
     <section id="products" className="max-container max-sm:mt-12">
       <div className="flex flex-col justify-start gap-5">
         <h2 className="text-4xl font-palanquin font-bold">
-          Our <span className="text-coral-red"> Popular </span> Products.
+          Our <span className="text-coral-red"> Popular </span> Products
         </h2>
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notchs quality and styles with our soughtvafter
-          selection. Discover a worlds of comforts, design, and valuee
+          selection. Discover a worlds of comforts, design, and value
         </p>
       </div>
 
