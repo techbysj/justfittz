9b560bd593ebfab7ddd97e47dd24217a31af6362
techbysj <jsabood19@gmail.com>
2021-10-15 19:07:32 +0100

    Final Review and Testing

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -10,7 +10,7 @@ const PopularProducts = () => {
         </h2>
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top-notchs quality and styles with our soughtvafter
-          selections. Discovering a worlds of comforts,  design, and value
+          selections. Discovering a world of comforts,  design, and value
         </p>
       </div>
 
