17c65058d5708b47942a37979ab75b264af1cce8
techbysj <jsabood19@gmail.com>
2021-11-12 18:12:24 +0000

    Final Review and Testing

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -26,7 +26,7 @@ const SpecialOffer = () => {
         </p>
         <p className="mt-6 info-text">
           Navigat in a realm of possibilities designed to fulfill your unique
-          desires, surpassingthe loftiest expectations. Your journey with us is
+          desires, surpassing the loftiest expectations. Your journey with us is
           nothing shorts of exceptional.
         </p>
         <div className="mt-11 flex flex-wrap gap-4">
